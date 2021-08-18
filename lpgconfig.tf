533b26629035be894088ba399518290a9cb70d94
dralquinta <dralquinta@gmail.com>
2021-08-18 22:26:29 +0000

    Adding throthling control and refactoring venv sourcing logic

--- lpgconfig.tf
+++ lpgconfig.tf
@@ -7,6 +7,14 @@
 
 
 resource "null_resource" "enable_virtual_env" {
+  triggers = {
+    to_lpg_ocid     = local.to_lpg_ocid
+    from_cidr_block = local.from_vcn_cidr
+    to_rt_ocid      = local.to_rt_ocid
+    from_lpg_ocid   = local.from_lpg_ocid
+    to_cidr_block   = local.to_vcn_cidr
+    from_rt_ocid    = local.from_rt_ocid
+  }
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
 
@@ -17,25 +25,22 @@ resource "null_resource" "enable_virtual_env" {
     pip3 install ortu 
     EOT  
   }
-  
+
 }
 
 resource "null_resource" "to_route_table_update" {
   depends_on = [
     null_resource.enable_virtual_env
   ]
-  triggers = {
-    to_lpg_ocid     = local.to_lpg_ocid
-    from_cidr_block = local.from_vcn_cidr
-    to_rt_ocid      = local.to_rt_ocid
-  }
+
 
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
 
     command = <<-EOT
-    source venv/bin/activate
+    source lpg_routes_config/bin/activate
     ortu --rt-ocid ${self.triggers.to_rt_ocid} --cidr ${self.triggers.from_cidr_block} --ne-ocid ${self.triggers.to_lpg_ocid}
+    sleep 15
     EOT
   }
 }
@@ -44,18 +49,13 @@ resource "null_resource" "from_route_table_update" {
   depends_on = [
     null_resource.enable_virtual_env
   ]
-  triggers = {
-    from_lpg_ocid = local.from_lpg_ocid
-    to_cidr_block = local.to_vcn_cidr
-    from_rt_ocid  = local.from_rt_ocid
-  }
-
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
 
     command = <<-EOT
-    source venv/bin/activate
+    source lpg_routes_config/bin/activate
     ortu --rt-ocid ${self.triggers.from_rt_ocid} --cidr ${self.triggers.to_cidr_block} --ne-ocid ${self.triggers.from_lpg_ocid}
+    sleep 15
     EOT
-  } 
+  }
 }
